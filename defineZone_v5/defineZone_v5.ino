69c7dd424778800257d877f5cf906a93ae193bdd
jakob0610 <169160456+jakob0610@users.noreply.github.com>
2024-05-13 13:22:01 +0200

    Update defineZone_v5.ino
    added SET-logic
       bool emergency
      Power Button

--- defineZone_v5/defineZone_v5.ino
+++ defineZone_v5/defineZone_v5.ino
@@ -47,11 +47,15 @@ const int RESET_BUTTON = 5;
 // Button pin for setting the system
 const int SET_BUTTON = 6; 
 
+
 // Motor pin for clockwise rotation
 const int MOT_CLOCK = 7; 
 
 // Motor pin for antilockwise rotation
 const int MOT_ACLOCK = 8; 
+
+// Button pin for running the chain
+const int POWER_BUTTON = 9; 
 
 // Alpha value for complementary filter
 double ALPHA = 0.75; 
@@ -104,16 +108,19 @@ double elapsedTime;
 
 // Boolean variable to check if the chainsaw should be running
 bool chainsawRunning;
+bool emergency=false;
 
-enum State {
-  NORMAL,
+enum State 
+{
+  RUNNING,
+  STANDBY,
   RESET,
   SET,
   EMERGENCY,
   STOP
 };
 
-State currentState=NORMAL;
+State currentState=STANDBY;
 
 void updateState(State newState) 
 {
@@ -165,6 +172,7 @@ void setup()
   // Set Pins for Button as Inputs
   pinMode(RESET_BUTTON, INPUT_PULLUP);
   pinMode(SET_BUTTON, INPUT_PULLUP);
+    pinMode(POWER_BUTTON, INPUT_PULLUP);
 
   // Set Pins for Motor as Outputs
   pinMode(MOT_CLOCK, OUTPUT);
@@ -174,53 +182,24 @@ void setup()
       setMaxValues(PRESET_PITCH_MAX, PRESET_ROLL_MAX,PRESET_YAW_MAX,PRESET_COMBINED_MAX);
       setMinValues(PRESET_PITCH_MIN, PRESET_ROLL_MIN,PRESET_YAW_MIN);
 
-  updateState(NORMAL);
+  updateState(STANDBY);
   blink();
   chainsawRunning=false;
 }
 
-void checkAndUpdate(float currentMax, float currentMin, float finalValue) {
-  if(finalValue > currentMax) {
-    currentMax = finalValue;
-  }
-  if(finalValue < currentMin) {
-    currentMin = finalValue;
-  }
-}
-
-void checkValues() {
-  checkAndUpdate(Pitch_Max, Pitch_Min, pitch_final);
-  checkAndUpdate(Roll_Max, Roll_Min, roll_final);
-  checkAndUpdate(Yaw_Max, Yaw_Min, yaw_final);
-}
-
-void checkButtonAndSetState(int button, int state) {
-  if(digitalRead(button) == LOW) {
-    updateState(state);
-  }
-}
-
-void checkValueAndSetEmergency(float value, float min, float max, const char* message) 
-{
-  if(chainsawRunning && (value < min || value > max)) {
-    Serial.println(message);
-    Serial.print("Value:");
-        Serial.println(value);
-        Serial.print("MIN:");
-        Serial.println(min);
-        Serial.print("MAX:");
-        Serial.println(max);
-        Serial.println();
-    updateState(EMERGENCY);
-  }
-}
 
 void loop()
 {
-
+  //Checking if a button is pressed
   checkButtonAndSetState(RESET_BUTTON, RESET);
-  checkButtonAndSetState(SET_BUTTON, SET);
 
+  if(!emergency) //check if the emergency is activated
+  {
+    checkButtonAndSetState(SET_BUTTON, SET);
+    checkButtonAndSetState(POWER_BUTTON, RUNNING);
+  }
+
+  //checking if the current Values of Pitch, Roll, Yaw & Combined, are in the set limits
   checkValueAndSetEmergency(pitch_final, Pitch_Min, Pitch_Max, "PITCH_OUT_OF_RANGE");
   checkValueAndSetEmergency(roll_final, Roll_Min, Roll_Max, "ROLL_OUT_OF_RANGE");
   checkValueAndSetEmergency(yaw_final, Yaw_Min, Yaw_Max, "YAW_OUT_OF_RANGE");
@@ -230,17 +209,29 @@ void loop()
 
   switch(currentState)
   {
-    case NORMAL:   //normal use
+    case RUNNING:   //chainsaw is activ, trigger is pressed-> chain is moving
       chainsawRunning=true;
       digitalWrite(WORK_LED, HIGH);
       digitalWrite(BRAKE_LED, LOW);
       digitalWrite(SETTING_LED, LOW);
       setAngles();
       printAngles();
-      break;
+      startVibration();
+      updateState(STANDBY);
+    break;
+
+    case STANDBY:  //chainsaw is activ, chain is not moving
+      chainsawRunning=false;
+      digitalWrite(WORK_LED, HIGH);
+      digitalWrite(BRAKE_LED, LOW);
+      digitalWrite(SETTING_LED, LOW);
+      stopVibration();
+    break;
+
 
     case RESET: //reset Button - reset YAW & goes to normal use
-    chainsawRunning=false;
+      chainsawRunning=false;
+      emergency=false;
       digitalWrite(WORK_LED, LOW);
       digitalWrite(BRAKE_LED, HIGH);
       digitalWrite(SETTING_LED, HIGH);
@@ -250,47 +241,53 @@ void loop()
       setAngles();
       setMaxValues(PRESET_PITCH_MAX, PRESET_ROLL_MAX,PRESET_YAW_MAX,PRESET_COMBINED_MAX);
       setMinValues(PRESET_PITCH_MIN, PRESET_ROLL_MIN,PRESET_YAW_MIN);
-      updateState(NORMAL);
+      updateState(STANDBY);
       stopVibration();
-      break;
+    break;
 
     case SET: //Set-Button - defining ZONE
       chainsawRunning=false;
+      stopVibration();
       digitalWrite(WORK_LED, LOW);
       digitalWrite(BRAKE_LED, LOW);
       digitalWrite(SETTING_LED, HIGH);
 
-     /* if(Pitch_Max==PRESET_PITCH_MAX && Roll_Max==PRESET_ROLL_MAX && Yaw_Max==PRESET_YAW_MAX && Pitch_Min==PRESET_PITCH_MIN && Roll_Min==PRESET_ROLL_MIN && Yaw_Min==PRESET_YAW_MIN)
+
+      if(Pitch_Max==PRESET_PITCH_MAX && Roll_Max==PRESET_ROLL_MAX && Yaw_Max==PRESET_YAW_MAX && Pitch_Min==PRESET_PITCH_MIN && Roll_Min==PRESET_ROLL_MIN && Yaw_Min==PRESET_YAW_MIN)
       {
-        setMaxValues(5, 5,5, 10);
+        setMaxValues(5, 5, 5, 10);
         setMinValues(-5, -5, -5);
-      }*/
-startVibration();
+      }
+
       setAngles();
       checkValues();
-      printAngles();
-      updateState(NORMAL);
-      break;
+      printMinMaxValues();
+      //printAngles();
+      updateState(STANDBY);
+    break;
 
     case EMERGENCY: //Emergency-STOP
+    emergency=true;
       chainsawRunning=false;
       stopVibration();
       digitalWrite(WORK_LED, LOW);
       digitalWrite(BRAKE_LED, HIGH);
       digitalWrite(SETTING_LED, LOW);
-      updateState(STOP);
-      break;
+      //updateState(STOP);
+    break;
 
   }
 }
 
-void setLEDs(int state) {
+void setLEDs(int state) 
+{
   digitalWrite(WORK_LED, state);
   digitalWrite(BRAKE_LED, state);
   digitalWrite(SETTING_LED, state);
 }
 
-void blink() {
+void blink() 
+{
   for(int i = 0; i < 2; i++) {
     setLEDs(HIGH);
     delay(200);
@@ -301,8 +298,8 @@ void blink() {
 
 void setAngles()
 {
-if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) // Get the Latest packet 
-{ 
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) // Get the Latest packet 
+  { 
 
         // display quaternion values in easy matrix form: w x y z
         mpu.dmpGetQuaternion(&q, fifoBuffer);
@@ -325,7 +322,7 @@ if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) // Get the Latest packet
         roll_final=ypr[2] * RAD_TO_DEG;
         combined_final = abs(pitch_final) + abs(yaw_final) + 1.5 * abs(roll_final);
 
-}
+  }
 }
 
 void printWithTab(const String& text, float value) 
@@ -335,23 +332,23 @@ void printWithTab(const String& text, float value)
   Serial.println();
 }
 
-void printAngles() 
+void printAngles()  //prints the current Values of Pitch, Roll & Yaw
 {
   printWithTab("Pitch:", pitch_final);
   printWithTab("Roll:", roll_final);
   printWithTab("YAW:", yaw_final);
   printWithTab("COMBINED:", combined_final);
+}
 
-  // Uncomment the following lines if you want to print MAX and MIN values
-  /*
-  printWithTab("Pitch_Max:", PITCH_MAX);
-  printWithTab("Roll_MAX:", ROLL_MAX);
-  printWithTab("YAW_Max:", YAW_MAX);
-  printWithTab("Combined_Max:", COMBINED_MAX);
-  printWithTab("Pitch_Min:", PITCH_MIN);
-  printWithTab("Roll_Min:", ROLL_MIN);
-  printWithTab("YAW_Min:", YAW_MIN);
-  */
+void printMinMaxValues() //prints the current Values of MIN & MAx Values for Pitch, Roll & Yaw
+{
+  printWithTab("Pitch_Max:", Pitch_Max);
+  printWithTab("Roll_MAX:", Roll_Max);
+  printWithTab("YAW_Max:", Yaw_Max);
+  printWithTab("Combined_Max:", Combined_Max);
+  printWithTab("Pitch_Min:", Pitch_Min);
+  printWithTab("Roll_Min:", Roll_Min);
+  printWithTab("YAW_Min:", Yaw_Min);
 }
 
 void setMaxValues(double pitch_max, double roll_max, double yaw_max, double combined_max)
@@ -371,8 +368,6 @@ void setMinValues(double pitch_min, double roll_min, double yaw_min)
 
 }
 
-
-
 void startVibration()
 {
   digitalWrite(MOT_CLOCK,HIGH);
@@ -382,4 +377,53 @@ void stopVibration()
 {
   digitalWrite(MOT_CLOCK,LOW);
   digitalWrite(MOT_ACLOCK,LOW);
+}
+
+void checkAndUpdatePitch() 
+{
+  if(pitch_final > Pitch_Max) Pitch_Max = pitch_final;
+  if(pitch_final < Pitch_Min) Pitch_Min = pitch_final;
+}
+
+void checkAndUpdateRoll() 
+{
+  if(roll_final > Roll_Max) Roll_Max = roll_final;
+  if(roll_final < Roll_Min) Roll_Min = roll_final;
+}
+
+void checkAndUpdateYaw() 
+{
+  if(yaw_final > Yaw_Max) Yaw_Max = yaw_final;
+  if(yaw_final < Yaw_Min) Yaw_Min = yaw_final;
+}
+
+void checkValues() 
+{
+  checkAndUpdatePitch();
+  checkAndUpdateRoll();
+  checkAndUpdateYaw();
+  Combined_Max = abs(Pitch_Max) + abs(Yaw_Max) + 1.5 * abs(Roll_Max);
+}
+
+void checkButtonAndSetState(int button, int state) 
+{ //HIGH && last_state == true
+  if(digitalRead(button) == LOW) 
+  {
+    updateState(state);
+  }
+}
+
+void checkValueAndSetEmergency(float value, float min, float max, const char* message) 
+{
+  if(chainsawRunning && (value < min || value > max)) {
+    Serial.println(message);
+    Serial.print("Value:");
+        Serial.println(value);
+        Serial.print("MIN:");
+        Serial.println(min);
+        Serial.print("MAX:");
+        Serial.println(max);
+        Serial.println();
+    updateState(EMERGENCY);
+  }
 }
